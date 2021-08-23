d1e09b79847aea7e97ea08cb634c335245558b06
akash345 <akashmandavilli@gmail.com>
2021-08-22 20:28:53 -0400

    Trying to get AirTable API to work

--- popup.js
+++ popup.js
@@ -1,14 +1,14 @@
 //Capture submit button
 document.getElementById("addContact").addEventListener("click",function() {
-    var fname = document.getElementById("fname").value;
+    let fname = document.getElementById("fname").value;
   //  alert(fname);
-    var lname = document.getElementById("lname").value;
-    var company = document.getElementById("company").value;
-    var title = document.getElementById("title").value;
+    let lname = document.getElementById("lname").value;
+    let company = document.getElementById("company").value;
+    let title = document.getElementById("title").value;
    // alert(fname + " " + lname + " " + company + " " + title);
    //Get contact status
-    var contactStatus;
-    var contactstatus = document.getElementsByName('contact status');
+    let contactStatus;
+    let contactstatus = document.getElementsByName('contact status');
 
     for(let i = 0; i < contactstatus.length; i++) {
         if(contactstatus[i].checked)
@@ -27,5 +27,34 @@ document.getElementById("addContact").addEventListener("click",function() {
             contactMode = document.getElementById(contactmode[i].id).value;
         }
     }
-    alert(contactMode)
+  //  alert(contactMode)
+    const Airtable = require('airtable');
+    const base = new Airtable({ apiKey: '<keyo3cFjinZv8ziCe>' }).base(
+        'appBrHVddsAPjVnXh'
+    );
+
+    const table = base('Table 1');
+    base('Table 1').create([{
+
+            "fields": {
+                "First Name": fname,
+                "Last Name": lname,
+                "Company": company,
+                "Job Title": title,
+                "Mode of Contact": contactMode,
+                "Connection Status": contactStatus
+            }}
+        ]
+
+    , {typecast: true}, function(err, records) {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        records.forEach(function (record) {
+            console.log(record.getId());
+        });
+    });
 })
+
+
